4980c68ee88ba9e726d3bee72e3d7c768473eef3
eshwar3214 <eshwar.smart2001@gmail.com>
2021-02-02 21:54:51 +0530

    added 8th program

---
+++ pythonassignment/8thprogram.py
@@ -0,0 +1,15 @@
+a=[]
+size=eval(input("enter size of the list:"))
+n=eval(input("enter a number:"))
+
+
+for i in range(size):
+    a.append(eval(input("a["+str(i)+"]")))
+
+def greater_than_n(list,number):
+    print("elements of the list greater than",n,"are")
+    for i in range(size):
+        if a[i]>n:
+            print(a[i])
+
+greater_than_n(a,n)
