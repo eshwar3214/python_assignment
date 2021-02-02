75af0fdc5513b12c38321a3098cc3743c5dce6c3
eshwar3214 <eshwar.smart2001@gmail.com>
2021-02-02 21:44:00 +0530

    added some modifications to program 2

--- pythonassignment/2ndprogram.py
+++ pythonassignment/2ndprogram.py
@@ -1,7 +1,10 @@
 n=eval(input("enter the set size:"))
+
 a=[]
+
 for i in range(n):
     a.append(eval(input("enter values a["+str(i)+"]:")))
+
 
 a.sort()
 print("largest number in the list is",a[-1])
