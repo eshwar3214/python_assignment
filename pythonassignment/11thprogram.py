6f47c8ac0039d20c5559d94e20f100979d6301c3
eshwar3214 <eshwar.smart2001@gmail.com>
2021-02-03 12:21:03 +0530

    added comments

--- pythonassignment/11thprogram.py
+++ pythonassignment/11thprogram.py
@@ -1,3 +1,7 @@
+# name   : sai eshwar reddy kottapally
+# rollno : 100519733022
+# program to accept two strings and print whether they are equal
+
 s1=input("enter a string:")
 s2=input("enter another string:")
 
@@ -5,4 +9,3 @@ if s1==s2:
     print("yes they are equal")
 else:
     print("they are not equal")
-    
