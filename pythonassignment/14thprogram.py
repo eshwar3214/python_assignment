b8b263e9b913f7929bb5a930d85c415c6feb215b
eshwar3214 <eshwar.smart2001@gmail.com>
2021-02-03 20:04:17 +0530

    added 14thprogram

---
+++ pythonassignment/14thprogram.py
@@ -0,0 +1,12 @@
+# name    : sai eshwar reddy kottapally
+# rollno  : 100519733022
+# program with a function that takes two lists and outputs pairwise sum of the lists
+
+list1=[1,2,3,4,5]
+list2=[6,7,8,9,10]
+list3=[]
+
+for i in range(len(list1)):
+    list3.append(list1[i]+list2[i])
+
+print(list3)
