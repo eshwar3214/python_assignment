4957d0386077e6a88a9a55533cd6328576735d53
eshwar3214 <eshwar.smart2001@gmail.com>
2021-02-03 20:20:19 +0530

    added addlist function

--- pythonassignment/14thprogram.py
+++ pythonassignment/14thprogram.py
@@ -4,9 +4,13 @@
 
 list1=[1,2,3,4,5]
 list2=[6,7,8,9,10]
-list3=[]
 
-for i in range(len(list1)):
-    list3.append(list1[i]+list2[i])
 
-print(list3)
+def addlists(a,b):
+    c=[]
+    for i in range(len(a)):
+         c.append(a[i]+b[i])
+    return c
+
+print(addlists(list1,list2))
+
