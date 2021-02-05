6258df9f2df319df5fe763daf987dbdc632c01de
eshwar3214 <eshwar.smart2001@gmail.com>
2021-02-05 11:16:59 +0530

    added 17th program

--- pythonassignment/17thprogram.py
+++ pythonassignment/17thprogram.py
@@ -1,11 +1,11 @@
 # name    : sai eshwar reddy kottapally
 # rollno  : 100519733022
-# program to find the second largest number of a list
+# program to find the second smallest number of a list
 
-list=[2,2,4,5,6,7,8,9,22,19]
+list=[5,4,5,6,7,8,9,10,44,3]
 
 def secondsmallest(a):
     a.sort()
-    print(a[a.index(max(a))-1])
+    print(a[a.index(min(a))+1])
 
 secondsmallest(list)
